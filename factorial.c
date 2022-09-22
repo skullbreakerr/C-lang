72408d357f836c5bfab29fe13c53f598c40b0e35
skullbreakerr <pateldharmiiik2002@gmail.com>
2022-09-22 17:45:59 +0530

    save commit :   factorial.c

--- factorial.c
+++ factorial.c
@@ -1,7 +1,8 @@
 #include<stdio.h> 
-#include<conio.h>           //factorial 4!= 4x3x2x1===>>RECURSIVELY!!//
+#include<conio.h>
+#include<stdlib.h>           //factorial 4!= 4x3x2x1===>>RECURSIVELY!!//
 int fact(int n){
-   if (n==1) return n; 
+   if (n==1) return n;
    return n*fact(n-1);    
 }
 int main(){
